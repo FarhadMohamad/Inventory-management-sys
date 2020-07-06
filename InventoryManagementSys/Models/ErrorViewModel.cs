0935f7c7198417f622852bce42e9f4bb5ce404d8
FarhadMohamad <art_ismat@hotmail.com>
2020-07-06 13:44:39 +0200

    Project setup

---
+++ InventoryManagementSys/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InventoryManagementSys.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
