d83cac7391f60355d94eaa2cc871fcca0439155d
atmar kohistany <atmar@hotmail.dk>
2020-07-21 02:05:50 +0200

    Register and login modification

--- InventoryManagementSys/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ InventoryManagementSys/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -95,6 +95,7 @@ namespace InventoryManagementSys.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+                    
                     await _userManager.AddToRoleAsync(user, role.Name);
 
                     //        var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
